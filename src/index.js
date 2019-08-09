941edfd1d18f1ea83381395d618c11748495df91
anamika8 <anamikaeight@gmail.com>
2019-08-09 11:56:33 -0700

    added first component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 
-import Game from "./components/game";
+import Game from "./components/Game";
 
 render(<Game />, document.getElementById("root"));
