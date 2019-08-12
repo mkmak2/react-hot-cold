2fee2002a27d562d6b1557fc9f602b91b54021b6
anamika8 <anamikaeight@gmail.com>
2019-08-11 20:24:58 -0700

    completed the drill

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,9 +11,13 @@ import "./Game.css";
 class Game extends React.Component {
   constructor(props) {
     super(props);
+    const correctAnswer = Math.floor(Math.random() * 100) + 1;
+    console.log(correctAnswer);
 
     this.state = {
-      guesses: {}
+      guesses: {},
+      correctAnswer: correctAnswer,
+      index: ""
     };
   }
 
@@ -21,30 +25,38 @@ class Game extends React.Component {
     // 1. Take a copy of the existing state
     const guesses = { ...this.state.guesses };
     // 2. Add our new input to that inputs variable
-    guesses[`Guess#${Date.now()}`] = guess;
+    const index = `Guess#${Date.now()}`;
+    guesses[index] = guess;
     // 3. Set the new userInputs object to state
-    this.setState({ guesses });
+    const correctAnswer = this.state.correctAnswer;
+    this.setState({ guesses, correctAnswer, index });
   };
 
   render() {
     return (
       <div className="Hot-or-Cold">
         <Header />
-        <Response />
-        <div className="input-section">
-          <UserInput enterInput={this.enterInput} />
-          <section className="counter-section">
-            <GuessCounter numberOfGuesses={this.state.guesses} />
-          </section>
-          <section className="guesses">
-            {Object.keys(this.state.guesses).map(key => (
-              <UserAnswers
-                key={key}
-                index={key}
-                answers={this.state.guesses[key]}
-              />
-            ))}
-          </section>
+        <div className="app">
+          <Response
+            allGuessses={this.state.guesses}
+            correctAnswer={this.state.correctAnswer}
+            index={this.state.index}
+          />
+          <div className="input-section">
+            <UserInput enterInput={this.enterInput} />
+            <section className="counter-section">
+              <GuessCounter numberOfGuesses={this.state.guesses} />
+            </section>
+            <section className="guesses">
+              {Object.keys(this.state.guesses).map(key => (
+                <UserAnswers
+                  key={key}
+                  index={key}
+                  answers={this.state.guesses[key]}
+                />
+              ))}
+            </section>
+          </div>
         </div>
       </div>
     );
--- src/components/Response.js
+++ src/components/Response.js
@@ -1,8 +1,32 @@
 import React from "react";
+import "./Response.css";
 
 class Response extends React.Component {
+  hotOrCold = (correctAnswer, lastGuessValue) => {
+    let difference = Math.abs(lastGuessValue - correctAnswer);
+    let feedback;
+    if (difference == 0) {
+      feedback = "YOU GOt IT!";
+    } else if (difference <= 5) {
+      feedback = "HOT";
+    } else if (difference <= 15) {
+      feedback = "WARM";
+    } else {
+      feedback = "COLD";
+    }
+    return feedback;
+  };
+
   render() {
-    return <div className="response">Response!!!</div>;
+    const { allGuessses, correctAnswer, index } = this.props;
+    const lastGuess = allGuessses[index];
+    let feedback = "";
+    if (lastGuess) {
+      const lastGuessValue = lastGuess["inputs"];
+      feedback = this.hotOrCold(correctAnswer, lastGuessValue);
+    }
+
+    return <div className="response">{feedback}</div>;
   }
 }
 
